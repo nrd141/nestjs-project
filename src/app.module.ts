42d647bf22f38aeef271c414e6148b10c0333873
naufaldi <nrd141@gmail.com>
2024-08-03 09:16:12 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true, 
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/book/book.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+
+@Controller('books')
+export class BookController {
+    constructor(private bookService: BookService){}
+
+    @Get()
+    async getAllBooks(): Promise<Book[]> {
+        return this.bookService.findAll();
+    }
+
+    @Post()
+    async createBook(
+        @Body()
+        book: CreateBookDto
+    ): Promise<Book> {
+        return this.bookService.create(book);
+    }
+
+    @Get(':id')
+    async getBook(
+        @Param('id')
+        id: string,
+    ): Promise<Book> {
+        return this.bookService.findById(id);
+    }
+}
